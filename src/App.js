e8070ae39e79b2a0ef2f5d50ec7ce210138ad883
Хацаев Адам <hacaevadam@gmail.com>
2022-07-26 18:39:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
+import React from 'react';
+import axios from 'axios';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+
+
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSerchValue] = React.useState('');
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    axios.get('https://62dd641f79b9f8c30aa89ac3.mockapi.io/items').then((res) => {
+      setItems(res.data);
+    });
+    axios.get('https://62dd641f79b9f8c30aa89ac3.mockapi.io/cart').then((res) => {
+      setCartItems(res.data);
+    });
+
+  }, []);
+
+ const onAddToCart = (obj) => {
+  axios.post('https://62dd641f79b9f8c30aa89ac3.mockapi.io/cart', obj);
+  setCartItems(prev => [...prev, obj]);
+ };
+
+ const onRemoveItem = (id) => {
+  // axios.delete(`https://62dd641f79b9f8c30aa89ac3.mockapi.io/cart/${id}`);
+  setCartItems(prev => prev.filter(item => item.id ===id));
+  };
+ 
+ const onChangeSearchInput = (event) => {
+  setSerchValue(event.target.value);
+ };
+ 
+
   return (
     <div className="wrapper clear">
      
-      <Drawer/>
-      <Header/>
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
+      <Header onClickCart={() => setCartOpened(true)}/>
       <div className="content p-40">
 
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ? `Поиск по запросу:"${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Пoиск..." />
+            {searchValue && (
+              <img 
+                onClick={() => setSerchValue('')}
+                className="clear cu-p" 
+                src="/img/btn-remove.svg" 
+                alt="clear" 
+              />
+            )}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Пoиск..." />
           </div>
         </div>
       
        
-        <div className="d-flex">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>          
+        <div className="d-flex flex-wrap">
+          {items
+          .filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((item, index) => (
+            <Card 
+              key={index}
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log('Добавили в закладки')}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}        
         </div>
       </div>
     </div>
---
+++ src/components/Card/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './Card.module.scss';
+
+function Card({imageUrl, title, price, onFavorite, onPlus}) {
+
+    const [isAdded, setIsAdded]=React.useState(false);
+
+    const onClickPlus = () => {
+        onPlus({title, imageUrl, price});
+        setIsAdded(!isAdded)
+    };
+
+    return(
+    <div className={styles.card}>
+        <div className={styles.favorite} onClick={onFavorite}>
+            <img src="/img/heart-red.svg" alt="heart" />
+        </div>
+        <img width={133} height={112} src={imageUrl} alt="sneakers" />
+        <h5>{title}</h5>
+        <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>{price} руб.</b>
+            </div>
+            
+                <img
+                 className={styles.plus}
+                 onClick={onClickPlus}  
+                 src={isAdded ? '/img/btn-cheked.svg' : '/img/btn-plus.svg'}
+                 alt="plus"
+                 />
+            
+        </div>
+    </div>
+    );
+}
+
+export default Card;
+
